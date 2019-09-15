66c29d6b6a1dabeb41fde926ed0bd4f2bf62ceb8
Peter Denno <podenno@gmail.com>
2019-09-14 20:47:12 -0400

    integrate with mznb

--- nba_gateway/nba_gateway.py
+++ nba_gateway/nba_gateway.py
@@ -3,11 +3,17 @@ import zmq
 import json
 import sys
 import asyncio
-
-class NBAgateway(object):
-    def __init__(self,port):
-        self.port = port
-        self.endpoint = 'tcp://*:' + str(port)
+import socket
+from contextlib import closing
+    
+class NBAgateway():
+    def __init__(self):
+        # https://stackoverflow.com/questions/1365265/on-localhost-how-do-i-pick-a-free-port-number
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+            s.bind(('', 0))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.port = s.getsockname()[1]
+        self.endpoint = 'tcp://*:' + str(self.port)
         self.sock = None
         self.keep_running = False
         self.ctx = zmq.Context()
@@ -15,16 +21,13 @@ class NBAgateway(object):
         self.sock.bind(self.endpoint)
 
     async def listen(self):
-        print('NBAgateway listening on port', self.port)
-        print('context is %s' % (self.ctx,))
-        print('socket is %s' % (self.sock,))
+        print('NB Agent Gateway (nba_gateway) listening on port', self.port)
         self.keep_running = True
         while self.keep_running:
             try:
                 await asyncio.sleep(0.5)
                 try:
-                    msg = self.sock.recv(flags=zmq.NOBLOCK)
-                    print('Past the recv()!!!!! msg = %s' % (msg,))
+                    msg = self.sock.recv(flags=zmq.NOBLOCK) # await here doesn't work
                     msg = json.loads(msg)
                 except zmq.ZMQError as e:
                     msg = False
@@ -35,18 +38,15 @@ class NBAgateway(object):
                     elif (msg['cmd'] == 'put_val'):          # put_val
                         var = msg['var']
                         val = msg['val']
-                        module = sys.modules[__name__]
+                        module = sys.modules["__main__"]
                         setattr(module, var, val)
-                        globals()[var] = val
+                        globals()[var] = val # not sure why both are needed. They are.
                         self.sock.send_string('"OK"')
                     elif (msg['cmd'] == 'get_val'):          # get_val
                         var = msg['var']
-                        print('get_val: %s' % (var,))
                         if (var in (globals)()):
-                            print('found in globals: %s' % (globals()[var],))
                             self.sock.send_string(json.dumps(globals()[var]))
                         else:
-                            print('did not find %s' % (var,))
                             self.sock.send_string('"UNKNOWN_VAR"')
                     else:
                         self.sock.send_string('"UNKNOWN_CMD"')
